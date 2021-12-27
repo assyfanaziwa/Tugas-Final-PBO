fee1360848653857fbeafa698b5ae94b505afd51
assyfanaziwa <83399060+assyfanaziwa@users.noreply.github.com>
2021-12-27 14:21:29 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,28 @@
+package koperasi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Asets/view.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Koperasi Sejahtera Abadi");
+        stage.getIcons().add(new Image (Objects.requireNonNull(getClass().getResourceAsStream("Asets/logoKoperasi.png"))));
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+}

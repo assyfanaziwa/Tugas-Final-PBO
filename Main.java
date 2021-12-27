9c97e5c888be630b92b996812df34f9613494d65
Melan Caniadi <83539854+Melan120322@users.noreply.github.com>
2021-12-27 14:09:21 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-      public static void main(String[] args){
-            launch(args);
-        }
+    public static void main(String[] args){
+          launch(args);
+      }
 }
